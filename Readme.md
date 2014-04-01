9f6d3316f8324b4e81c370419db773181463b3a0
Eric Gjertsen <ericgj72@gmail.com>
2014-04-01 16:03:40 -0400

    reimplement; see Readme for changes

--- Readme.md
+++ Readme.md
@@ -1,7 +1,7 @@
 
 # Accordion
 
-  Basic (vertical) accordion component
+  Simple (vertical) accordion UI component
 
   <img alt="Example" src="http://i.imgur.com/KF8uNcR.png" />
 
@@ -11,48 +11,167 @@
 
     $ component install ericgj/accordion
 
-## Events
+## Usage
 
-  - `select(panel)`    when an accordion panel is selected
-  - `deselect(panel)`  when an accordion panel is deselected
+  There are two ways you can use this component to attach to the DOM.
+
+  1. _Passive_: No changes are made to the document structure; your document
+  already provides the necessary structure under the given element (see
+  below). This is the default. For example:
+
+  ```js
+  // set options
+  var accordion = Accordion()
+                    .multiselectable(true);
+  
+  // binding
+  var panels = accordion(el);
+  
+  // programmatic manipulation
+  panels.deselectAll();
+  panels.click(0);
+  ```
+
+  2. _Active_: Your document is converted to the necessary structure under
+  the given element, based on header tags (h1, h2, h3, etc).
+
+  ```js
+  // set options
+  var accordion = Accordion()
+                    .deselectable(false)
+                    .hlevel(3);
+  
+  // convert structure and binding
+  var panels = accordion('#container');
+  
+  // semantic event hooks
+  panels.on('select', function(el,i,e){
+    if (e) window.scrollTo(0, e.pageY - 20);
+  });
+  ```
+
+  The `hlevel` specifies that any _h3 sections_ will be converted for use as
+  accordion panels. (Note that it detects if the DOM conversion has already
+  been done, so it is safe to re-bind an accordion to the same containing
+  element).  More about this below.
+
+  This is useful if you have a flat document structure, but want to use the
+  `h*` elements as semantic markers for accordion panels. For instance,
+  this is the kind of structure typically generated from markdown documents.
+
 
 ## API
+
+  __Please note that the API has changed quite a bit from earlier 0.0.x 
+  versions. The old version can be found on the `original` git tag.__
   
-### Accordion(el, options)
+### Accordion()
 
-  Creates a new `Accordion` bound to the given DOM element or selector.
+  Creates an _accordion function_ that can then be used multiple times
+  on container elements you want to treat as accordions. Options are set
+  directly on this function.
 
-  It makes the following assumptions about the markup:
+### accordion.multiselectable({Boolean})
+
+  If true, allows expansion of more than one panel at a time (default false).
+
+### accordion.deselectable({Boolean})
+
+  If true, allows collapse of selected panels (default true).
+
+### accordion.hlevel({Integer})
+
+  Convert all sections of elements under given header level into [hAtom][hatom]
+  structure, before binding accordion. For instance, if `accordion.hlevel(3)`,
+  then all h3's are interpreted as _accordion panel headers_, and elements
+  below them as _accordion panel contents_.  
   
-  1. The given element contains one or more [hAtom][hatom] entries. That is,
-  `div` or `article` elements matched by `.hentry`.
+  For example a flat structure like:
 
-  2. Each entry has an `.entry-title` element (again following hAtom),
-  which is used as the accordion header. Typically this would be an
-  `h3` etc, but could also be a `header` that contains several inline-
-  block elements, etc.
+  ```html
+  <h1>Title</h1>
+  <h2>subtitle</h2>
+  <p>Introductory paragraph.</p>
+  <h3>First item</h3>
+  <p>Description of the first item.</p>
+  <p>Any number of elements.</p>
+  <h2>New section</h2>
+  <h3>Second item</h3>
+  <p>Any number of elements.</p>
+  ```
 
-  3. If you want a `caret` (CSS arrow), for convenience the built-in CSS
-  includes styles for this. Simply include a `<span class="caret"></span>`
-  within your `.entry-title` element.
+  becomes:
 
-  The following options are available:
+  ```html
+  <h1>Title</h1>
+  <h2>subtitle</h2>
+  <p>Introductory paragraph.</p>
+  <div class="hentry">
+    <h3 class="entry-title">
+      <span class="caret"></span>
+      First item
+    </h3>
+    <div class="entry-content">
+      <p>Description of the first item.</p>
+      <p>Any number of elements.</p>
+    </div>
+  </div>
+  <h2>New section</h2>
+  <div class="hentry">
+    <h3 class="entry-title">
+      <span class="caret"></span>
+      Second item
+    </h3>
+    <div class="entry-content">
+      <p>Any number of elements.</p>
+    </div>
+  </div>
+  ```
 
-  - `deselect {Boolean}`    allows collapse of selected panel (default true)
-  - `multiselect {Boolean}` allows expansion of more than one panel (default false)
+### accordion({String|Element})
 
-### Accordion#select(panel)
+  Attach accordion (and optionally convert structure) under given container
+  el or (document) selector.  In other words, bind click events to `.hentry
+  .entry-title` elements within container.
 
-  Programmatically select (expand) given panel
+  Returns a `panels` object which allows programmatic manipulation of
+  accordion and `select`, `deselect` event hooks, see below.
 
-### Accordion#deselect(panel)
 
-  Programmatically deselect (collapse) given panel
+### panels.select({Integer})
 
-### Accordion#deselectAll
+  Select (expand) given panel.
 
-  Deselect all panels
+### panels.deselect({Integer})
+
+  Deselect (collapse) given panel.
+
+### panels.selectAll()
+
+  Select (expand) all panels.
   
+### panels.deselectAll()
+
+  Deselect (collapse) all panels.
+ 
+### panels.selected({Integer})
+
+  True if given panel is currently selected, false if not.
+
+### panels.click({Integer})
+
+  Programmatically click given panel, triggering multiselectable/deselectable
+  behavior.
+
+### panels.on({String},{Function})
+
+  Define callback on `select` or `deselect` events. Callbacks are passed the 
+  panel (.hentry) element, the panel index, and the DOM click event. (Note 
+  that for programmatic select and deselect, the DOM event is undefined.)
+ 
+## Events
+
+  See `panels.on` above.
 
 ## License
 
--- index.js
+++ index.js
@@ -2,100 +2,220 @@
 
 var classes = require('classes')
   , Emitter = require('emitter')
-  , noop    = function() { };
+  , noop    = function() { }
+  , has = hasOwnProperty
 
 module.exports = Accordion;
 
-var defaults = {
-  deselect:    true,
-  multiselect: false,
-  entry:       '.hentry'
-};
-
-function Accordion(el,options) {
-  if (!(this instanceof Accordion)) return new Accordion(el,options);
-
-  options = merge(defaults, options || {})
-
-  this.selectBehavior   = (options.multiselect ? noop : this.deselectAll);
-  this.reselectBehavior = (options.deselect ? this.deselect : noop);
-  this.entrySelector = options.entry;
-  this.panels = [];
-  this.el = el;
-  if (el) this.bind(el);
-  this.deselectAll();
-  return this;
-}
-
-Accordion.prototype = new Emitter;
-
-Accordion.prototype.bind = function(el){
-  if (typeof el=='string') el = document.querySelector(el);
-  var items = el.querySelectorAll(this.entrySelector);
-  for (var i=0;i<items.length;++i) {
-    this.panels.push(new Panel(this, items[i]));
+/*
+ * Example usage:
+ *
+ *    var accordion = 
+ *      Accordion()
+ *        .multiselectable(true)
+ *        .deselectable(true)
+ *        .hlevel(3)
+ *
+ *    var a = accordion(el);
+ *    a.on('select', anotherfn);
+ *    a.deselectAll();
+ *
+ *    // overwrite options
+ *    a.multiselectable(false);
+ *
+ */
+function Accordion(){
+  var opts = {
+    multiselectable: false,
+    deselectable:    true,
+    hlevel:          null
   }
-  classes(el).add('accordion');
-  return this;
-}
 
-Accordion.prototype.deselectAll = function(){
-  for (var i=0;i<this.panels.length;++i) {
-    this.deselect(this.panels[i])
+  accordion.multiselectable = function(bool){
+    opts.multiselectable = bool;
+    return this;
   }
-  return this;
-}
 
-Accordion.prototype.select = function(panel){
-  if (panel.selected){
-    this.reselectBehavior(panel);
-  } else {
-    this.selectBehavior(panel);
-    classes(panel.el).add('selected');
-    panel.selected = true;
-    this.emit('select',panel);
+  accordion.deselectable = function(bool){
+    opts.deselectable = bool;
+    return this;
   }
-}
 
-Accordion.prototype.deselect = function(panel){
-  classes(panel.el).remove('selected');
-  panel.selected = false;
-  this.emit('deselect', panel);
-}
+  accordion.hlevel = function(lvl){
+    opts.hlevel = lvl;
+    return this;
+  }
 
+  function accordion(el){
+    if (typeof el=='string') el = document.querySelector(el);
+    var items = el.querySelectorAll('.hentry');
 
-function Panel(container,el) {
-  if (!(this instanceof Panel)) return new Panel(container,el);
+    // if accordion structure not already created, generate
+    // and add 'accordion' to class of el
 
-  this.container = container;
-  this.el = el;
-  this.selected = false;
-  if (el) this.bind(el);
-  return this;
-}
-
-Panel.prototype.bind = function(el){
-  var title = el.querySelector('.entry-title');
-  var self = this;
-  title.onclick = function(){ self.container.select(self) };
-}
-
-// private
-
-var has = Object.prototype.hasOwnProperty;
-
-var mergeInto = function(a,b){
-  for (var key in b) {
-    if (has.call(b,key)){
-      a[key] = b[key];
+    if (opts.hlevel && items.length == 0){    
+      gen(el, opts.hlevel);
+      classes(el).add('accordion');
+      items = el.querySelectorAll('.hentry');
     }
+
+    // set panels options
+    var panels = 
+      Panels()
+        .multiselectable(opts.multiselectable)
+        .deselectable(opts.deselectable)
+
+    // bind events
+    return panels(items);
   }
-  return a;
+
+  return accordion;
+
 }
 
-// non-mutating merge
-var merge = function(a,b){
-  var m = mergeInto({},a);
-  return mergeInto(m,b);
+function Panels(){
+  var items = [];
+  var emitter = new Emitter();
+
+  panels.multiselectable = function(bool){
+    opts.select   = (bool ? noop : this.deselectAll.bind(this))
+    return this;
+  }
+
+  panels.deselectable = function(bool){
+    opts.reselect = (bool ? this.deselect.bind(this) : noop)
+    return this;
+  }
+
+  panels.click = function(i,e){
+    var item = items[i]
+    if (this.selected(i)){
+      opts.reselect(i,e); // reselect behavior, e.g. deselect
+    } else {
+      opts.select(i);     // select pre-behavior, e.g. deselectAll
+      this.select(i,e);   // select
+    }
+    return this;
+  }
+ 
+  panels.select = function(i,e){
+    var item = items[i]
+    classes(item).add('selected');
+    emitter.emit('select', item, i, e);
+  }
+
+  panels.deselect = function(i,e){
+    var item = items[i]
+    classes(item).remove('selected');
+    emitter.emit('deselect', item, i, e);
+  }
+
+  panels.selected = function(i){
+    var item = items[i]
+    return item && classes(item).has('selected');
+  }
+
+  panels.selectAll = function(){
+    for (var i=0;i<items.length;++i){
+      this.select(i);
+    }
+    return this;
+  }
+
+  panels.deselectAll = function(){
+    for (var i=0;i<items.length;++i){
+      this.deselect(i);
+    }
+    return this;
+  }
+
+  panels.on = function(name,fn){
+    emitter.on(name,fn);
+    return this;
+  }
+
+  var opts = {
+    select:   panels.deselectAll.bind(panels),
+    reselect: noop
+  }
+
+  function panels(els){
+    items = [];
+    for (var i=0;i<els.length;++i) items.push(els[i]);
+    items.forEach( function(el,i){
+      var title = el.querySelector('.entry-title')
+      if (title) title.onclick = function(e){ panels.click(i,e); }
+    })
+    return panels;
+  }
+
+  return panels;
+}
+
+
+// utils
+// TODO extract this into separate component
+
+function hlevel(el){
+  return {
+    'H1': 1,
+    'H2': 2,
+    'H3': 3,
+    'H4': 4,
+    'H5': 5,
+    'H6': 6
+  }[el.nodeName];
+}
+
+function siblingEl(el){
+  if (has.call(el,'nextElementSibling')){
+    return el.nextElementSibling;
+  } else {
+    var e = el.nextSibling;
+    while (e && !(e.nodeType == 1)) e = e.nextSibling;
+    return e;
+  }
+}
+
+function gen(container,lvl){
+
+  var sel = container.querySelectorAll('h'+lvl)
+    , heads = []
+  for (var i=0;i<sel.length;++i) heads.push(sel[i]);
+  var sections = heads.map( function(head){
+    var el = head
+      , els = [head]
+    while (el = siblingEl(el)){
+      var ellvl = hlevel(el)
+      if (ellvl && ellvl <= lvl) break;
+      els.push(el);
+    }
+    return els;
+  })
+  
+  sections.forEach( function(els){
+    var title = els[0]
+    classes(title).add('entry-title');
+    if (!title.querySelector('.caret')) {
+      var caret = document.createElement('span');
+      classes(caret).add('caret');
+      title.insertBefore(caret,title.firstChild);
+    }
+
+    var contents = els.slice(1)
+    var div = document.createElement('div')
+    classes(div).add('entry-content');
+    for (var i=0;i<contents.length;++i) div.appendChild(contents[i]);
+
+    var hentry = document.createElement('div')
+    classes(hentry).add('hentry');
+
+    title.parentNode.insertBefore(hentry,title);
+
+    hentry.appendChild(title);
+    hentry.appendChild(div);
+
+  })
+
 }
 
